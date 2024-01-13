f4b8cd2b7a6715ada5b8d9a928c5cfc6efc20ca2
Saul Contêlo Batista <saulcbatista@gmail.com>
2024-01-13 14:05:55 -0300

    first commit

---
+++ src/main/java/com/saulProgramado/NunesSportsAPI/NunesSportsApiApplication.java
@@ -0,0 +1,13 @@
+package com.saulProgramado.NunesSportsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NunesSportsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NunesSportsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saulProgramado/NunesSportsAPI/NunesSportsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saulProgramado.NunesSportsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NunesSportsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
