966d3a08a146c9c6f6bc8ae7470d00a01d7820ba
Saul Contêlo Batista <saulcbatista@gmail.com>
2024-01-15 17:55:37 -0300

    adding the consult function

--- src/main/java/com/saulProgramado/NunesSportsAPI/controller/ProductController.java
+++ src/main/java/com/saulProgramado/NunesSportsAPI/controller/ProductController.java
@@ -1,13 +1,18 @@
 package com.saulProgramado.NunesSportsAPI.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.saulProgramado.NunesSportsAPI.domain.Product;
+import com.saulProgramado.NunesSportsAPI.domain.ProductConsultData;
 import com.saulProgramado.NunesSportsAPI.domain.ProductDetailsData;
 import com.saulProgramado.NunesSportsAPI.domain.ProductRegisterData;
 import com.saulProgramado.NunesSportsAPI.domain.ProductRepository;
@@ -29,6 +34,12 @@ public class ProductController {
 		repository.save(product);
 		
 		return ResponseEntity.created(null).body(new ProductDetailsData(product));
+	}
+	
+	@GetMapping
+	public ResponseEntity<Page<ProductConsultData>> consult(@PageableDefault(sort = {"name"}) Pageable pagination) {
+		var page = repository.findAll(pagination).map(ProductConsultData::new);
+		return ResponseEntity.ok(page);
 	}
 	
 }
---
+++ src/main/java/com/saulProgramado/NunesSportsAPI/domain/ProductConsultData.java
@@ -0,0 +1,9 @@
+package com.saulProgramado.NunesSportsAPI.domain;
+
+public record ProductConsultData(Long id, String name, String code, String describe, Double price) {
+	
+	public ProductConsultData(Product product) {
+		this(product.getId(), product.getName(), product.getCode(), product.getDescribe(), product.getPrice());
+	}
+
+}
